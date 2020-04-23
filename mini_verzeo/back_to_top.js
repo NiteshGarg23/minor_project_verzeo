93811b40b272afe85570cf42c04872538a536ece
NiteshGarg23 <47839824+NiteshGarg23@users.noreply.github.com>
2020-04-23 19:42:39 +0530

    Add files via upload

---
+++ mini_verzeo/back_to_top.js
@@ -0,0 +1,16 @@
+$(document).scroll(function() {
+    var height = $(document).height();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
